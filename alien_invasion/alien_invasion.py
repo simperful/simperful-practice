9980686cb8a55fe0dbf9ca66de7ec45f455be2a1
simperful <zspdwyyx@163.com>
2018-12-26 22:02:11 +0800

    添加了 Ship 类

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,10 +2,13 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
-    # 初始化游戏并创建一个屏幕对象
+    # 初始化Pygame,设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))    # 设置屏幕对象的大小
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))    # 设置屏幕对象的大小
     pygame.display.set_caption("Alien Invasion")
 
     # 开始游戏的主循环
@@ -15,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环都会重绘屏幕
+        screen.fill(ai_settings.bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()  # Update the full display Surface to the screen
